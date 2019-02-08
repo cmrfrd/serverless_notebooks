add9950facc3bc01c218f4eb90cec77b50f522c9
Alexander Comerford <iambubbi@localhost.localdomain>
2019-02-08 14:26:38 -0500

    Update minikube script with increased storage

--- bin/start_minikube.sh
+++ bin/start_minikube.sh
@@ -1,10 +1,9 @@
 minikube profile serverless_datascience
 
-
 minikube start \
 	 --memory=8192 \
 	 --cpus=4 \
-	 --disk-size=30GB \
+	 --disk-size=50GB \
 	 --kubernetes-version=v1.11.0 \
 	 --insecure-registry localhost:5000 \
 	 --profile serverless_datascience
